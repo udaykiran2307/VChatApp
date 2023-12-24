11f26c3a64657c7f2b1c3b48b4f93d71704adbb4
uday <udaykiranvalaboju@gmail.com>
2023-12-24 20:53:37 +0530

    icon completed

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,13 @@
-import { Button } from "@chakra-ui/react";
+
 import "./App.css";
 import { Route } from "react-router-dom";
 import Chat from "./Pages/chat"
 import Home from "./Pages/home";
 function App() {
   return (
-    <div className="App">
+    <div className ="App">
    <Route path='/' component={Home} exact/>
    <Route path='/chats' component={Chat}/>
-   
     </div>
   );
 }
--- frontend/src/Pages/home.js
+++ frontend/src/Pages/home.js
@@ -1,6 +1,30 @@
 /* eslint-disable import/no-anonymous-default-export */
 
-
-export default ()=>{
-    return(<div>Login</div>);
-}
+import { Container, Box, Text } from "@chakra-ui/react";
+import { ChatIcon } from "@chakra-ui/icons";
+export default () => {
+  return (
+    <Container maxW="xl" centerContent>
+      <Box
+        display="flex"
+        justifyContent="center"
+        alignItems="center"
+        p={3}
+        bg={"cyan"}
+        w="100%"
+        m="40px 0 15px 0"
+        borderRadius={"lg"}
+        borderWidth={"1px"}
+      >
+        <ChatIcon  color={"darkGreen"}/>
+        <Text textAlign={"center"} fontSize={"4xl"} color={"black"}>
+         
+          DeV-Chat
+          
+        </Text>
+        <ChatIcon  color={"darkGreen"}/>
+      </Box>
+      <Box></Box>
+    </Container>
+  );
+};
